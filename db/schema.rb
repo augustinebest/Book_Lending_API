63c85b07f0418cd9dadad2235534ef0016a10ca7
Augustine Igwe <augustusbest00@gmail.com>
2024-10-10 11:42:33 +0100

    Updated Schema from Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_07_151407) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_10_091456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
-    t.boolean "available"
+    t.boolean "available", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_books_on_title", unique: true
   end
 
   create_table "lendings", force: :cascade do |t|
