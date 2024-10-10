9fcd2362d97c96074b8901aef3999c78fc3661bf
Augustine Igwe <augustusbest00@gmail.com>
2024-10-10 11:42:14 +0100

    Added resources for Book Crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "authentication/signup"
   post "authentication/login"
+
+  resources :books, only: [:index, :create, :show, :update, :destroy]
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
